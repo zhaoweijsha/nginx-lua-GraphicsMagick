8a9d2c4cb7a65af29606e7678c4aa9c0cb9ef167
赵伟 <zhaowei_ha@126.com>
2017-09-07 14:46:10 +0800

    定期清除数据

--- crontab.sh
+++ crontab.sh
@@ -1,2 +1,3 @@
 # 凌晨2点执行，查找目录下面7天内没有被访问的文件并删除，释放空间
-0 2 * * * find /tmp/thumb -atime -7 | xargs rm -rf
+0 2 * * * find /opt/uploads/cache/thumb -atime -7 | xargs rm -rf
+0 3 * * * find /opt/tomcat-7-8090/logs -atime -7 | xargs rm -rf
